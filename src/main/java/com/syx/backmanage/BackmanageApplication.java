5e75d69771446683ff0400c8756b06dd30a43557
syx543210 <1765365472@qq.com>
2020-10-10 12:21:31 +0800

    后台管理java端创建

---
+++ src/main/java/com/syx/backmanage/BackmanageApplication.java
@@ -0,0 +1,13 @@
+package com.syx.backmanage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackmanageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackmanageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/syx/backmanage/BackmanageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syx.backmanage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackmanageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
